21fa8f4ca385c6d531f16ba83590c30f3808476e
unknown <debayan005@gmail.com>
2018-11-30 04:36:53 +0530

    database connection fix

--- Furniture_Shop/core/init.php
+++ Furniture_Shop/core/init.php
@@ -1,5 +1,13 @@
 <?php
-$db=mysqli_connect("127.0.0.1","root","","handy");
+$dsn = "pgsql:"
+. "host=ec2-54-225-196-122.compute-1.amazonaws.com;"
+. "dbname=d4fg7upqlssb3k;"
+. "user=yeixvihchwxhto;"
+. "port=5432;"
+. "sslmode=require;"
+. "password=7e45cf8acf0d4631a68c43c355167a1f980c9099477722439f28c95edbf65e27";
+
+$db = new PDO($dsn);
 if(mysqli_connect_errno()){
     echo'database connection failed!!! '.mysqli_connect_error();
     die();
