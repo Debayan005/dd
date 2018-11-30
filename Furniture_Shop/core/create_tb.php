90e163c048b6797ab0875f04a416a49f1608ffe0
unknown <debayan005@gmail.com>
2018-11-30 06:09:21 +0530

    jhik

--- Furniture_Shop/core/create_tb.php
+++ Furniture_Shop/core/create_tb.php
@@ -1,13 +1,14 @@
 <?php
 include './Furniture_Shop/core/init.php';
 echo $db;
-$s="create table brands (id int(10) not null Primary key,brand varchar(30))";
-$s1="create table catagory(id int(10) foreign key(id) references brand(id),catagory varchar(30) parent int(10))";
-$s2="create table products(id int(10) foreign key(id) references brand(id) title varchar(30),price decimal(10,2) list_price decimal(10,2),brand varchar(30),category varchar(30),image varchar(255),description text,feature tinyint(4))";
 try{
+$s="create table brands (id int(10) not null Primary key,brand varchar(30))";
+// $s1="create table catagory(id int(10) foreign key(id) references brand(id),catagory varchar(30) parent int(10))";
+// $s2="create table products(id int(10) foreign key(id) references brand(id) title varchar(30),price decimal(10,2) list_price decimal(10,2),brand varchar(30),category varchar(30),image varchar(255),description text,feature tinyint(4))";
+
     $db->exec($s);
-    $db->exec($s1);
-    $db->exec($s2);
+    // $db->exec($s1);
+    // $db->exec($s2);
 } catch(Exception $e){
     echo $e;
 }
