fd6764dffcf5a101597ddfc581bd982d9e4e4ffe
unknown <debayan005@gmail.com>
2018-11-30 06:01:30 +0530

    jhik

--- Furniture_Shop/core/create_tb.php
+++ Furniture_Shop/core/create_tb.php
@@ -1,6 +1,6 @@
 <?php
-require 'Furniture_Shop/core/init.php';
-echo $db;
+//require 'Furniture_Shop/core/init.php';
+echo "db";
 // $s="create table brands (id int(10) not null Primary key,brand varchar(30))";
 // $s1="create table catagory(id int(10) foreign key(id) references brand(id),catagory varchar(30) parent int(10))";
 // $s2="create table products(id int(10) foreign key(id) references brand(id) title varchar(30),price decimal(10,2) list_price decimal(10,2),brand varchar(30),category varchar(30),image varchar(255),description text,feature tinyint(4))";
